e660fea6a2a75cc82c9021883cc56af9e51b8b90
ArkhamKnightGPC <nazter57@usp.br>
2025-12-19 10:10:14 -0300

    :memo: refactor sv tbs

--- sv/basic_circuits/testbenches/tb_nmux21.sv
+++ sv/basic_circuits/testbenches/tb_nmux21.sv
@@ -1,37 +1,37 @@
 module tb_nmux21;
 
-  parameter int N = 32;
-  logic [N-1:0] a, b, z;
-  logic s;
+parameter int N = 32;
+logic [N-1:0] a, b, z;
+logic s;
 
-  nmux21 #(
-    .N(N)
-  ) dut (
-    .a(a),
-    .b(b),
-    .s(s),
-    .z(z)
-  );
+nmux21 #(
+  .N(N)
+) dut (
+  .a(a),
+  .b(b),
+  .s(s),
+  .z(z)
+);
 
-  initial begin
-    a = 32'hAAAAAAAA;
-    b = 32'hBBBBBBBB;
-    s = 1'b0;
+initial begin
+  a = 32'hAAAAAAAA;
+  b = 32'hBBBBBBBB;
+  s = 1'b0;
 
-    #1;
-    if (z == a)
-        $display("TEST s=0 PASS");
-    else
-        $fatal(1, "TEST s=0 FAIL");
+  #1;
+  if (z == a)
+      $display("TEST s=0 PASS");
+  else
+      $fatal(1, "TEST s=0 FAIL");
 
-    s = 1'b1;
-    #1;
-    if (z == b)
-        $display("TEST s=1 PASS");
-    else
-        $fatal(1, "TEST s=1 FAIL");
+  s = 1'b1;
+  #1;
+  if (z == b)
+      $display("TEST s=1 PASS");
+  else
+      $fatal(1, "TEST s=1 FAIL");
 
-    $finish;
-  end
+  $finish;
+end
 
 endmodule
--- sv/basic_circuits/testbenches/tb_zero_check.sv
+++ sv/basic_circuits/testbenches/tb_zero_check.sv
@@ -1,32 +1,32 @@
 module tb_zero_check;
 
-    localparam int N = 32;
-    logic[N-1:0] a = '0;
-    logic zero;
-    logic nzero;
+localparam int N = 32;
+logic[N-1:0] a = '0;
+logic zero;
+logic nzero;
 
-    zero_check #(
-        .N(N)
-    ) dut (
-        .a(a),
-        .zero(zero),
-        .nzero(nzero)
-    );
+zero_check #(
+    .N(N)
+) dut (
+    .a(a),
+    .zero(zero),
+    .nzero(nzero)
+);
 
-    initial begin
-        for (int i = 0; i < N; i++) begin
-            a[i] = 1'b1;
-            #1;
-            assert (zero == 1'b0 && nzero == 1'b1)
-                else $fatal(1, "TEST %0d FAIL", 2*i+1);
+initial begin
+    for (int i = 0; i < N; i++) begin
+        a[i] = 1'b1;
+        #1;
+        assert (zero == 1'b0 && nzero == 1'b1)
+            else $fatal(1, "TEST %0d FAIL", 2*i+1);
 
-            a[i] = 1'b0;
-            #1;
-            assert (zero == 1'b1 && nzero == 1'b0)
-                else $fatal(1, "TEST %0d FAIL", 2*i+2);
-        end
-        $display("ALL TESTS PASSED");
-        $finish;
+        a[i] = 1'b0;
+        #1;
+        assert (zero == 1'b1 && nzero == 1'b0)
+            else $fatal(1, "TEST %0d FAIL", 2*i+2);
     end
+    $display("ALL TESTS PASSED");
+    $finish;
+end
 
 endmodule
